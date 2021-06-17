614d66af01d42969467d8142ea4871cff4e721fe
Fenniche-Ouahab <fennicheouahab1993@gmail.com>
2021-06-17 13:52:08 +0200

    add view capsules

---
+++ src/app/models/capsules.model.spec.ts
@@ -0,0 +1,7 @@
+import { Capsules.Model } from './capsules.model';
+
+describe('Capsules.Model', () => {
+  it('should create an instance', () => {
+    expect(new Capsules.Model()).toBeTruthy();
+  });
+});
